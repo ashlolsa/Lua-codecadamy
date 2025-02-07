9abd4ae59cbb660f6de11042173c90a0f87c4e34
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 16:16:53 -0600

    Update Lesson #1.lua

--- Functions/Lesson #1.lua
+++ Functions/Lesson #1.lua
@@ -1,3 +1,21 @@
 --intro to functions.
 
 --https://www.codecademy.com/courses/learn-lua/lessons/lua_functions/exercises/intro-to-functions
+
+-- Walking
+distance = 26.2
+mph = 2
+time = distance / mph
+print("It will take " .. time .. " hours to walk.")
+
+-- Biking
+distance = 26.2
+mph = 10
+time = distance / mph
+print("It will take " .. time .. " hours to bike.")
+
+-- Driving
+distance = 26.2
+mph = 40
+time = distance / mph
+print("It will take " .. time .. " hours to drive.")
