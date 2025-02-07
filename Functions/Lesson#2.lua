baecb842f7490afacfc83b1a9f8b5647316371a6
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 16:28:24 -0600

    Create Lesson#2.lua

---
+++ Functions/Lesson#2.lua
@@ -0,0 +1,8 @@
+--Declaring Functions
+--https://www.codecademy.com/courses/learn-lua/lessons/lua_functions/exercises/declaring-functions
+function printShoppingCartTotal()
+subtotal = 100
+taxRate = 1.2
+total = subtotal * taxRate
+print(total)
+end
