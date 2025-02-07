f930a7877aec5f74ad32c232fe2a55364c1380da
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 14:56:10 -0600

    Create Lesson #4.lua

---
+++ Conditionals & Logic/Lesson #4.lua
@@ -0,0 +1,17 @@
+--The elseif Statement
+--https://www.codecademy.com/courses/learn-lua/lessons/conditionals-logic-Lua/exercises/the-elseif-statement
+
+score = 100
+--Add your code here
+if score == 100 then
+print("Winner winner chicken dinner")
+
+elseif score > 80 then
+print("Close but not close enough")
+
+elseif score >= 60 then
+print("Try again")
+
+else
+print("Were you even playing?")
+end
