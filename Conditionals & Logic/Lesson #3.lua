f5645061e3cce1da6a3a235d40ce4abf35876dcb
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 14:47:24 -0600

    Update Lesson #3.lua

--- Conditionals & Logic/Lesson #3.lua
+++ Conditionals & Logic/Lesson #3.lua
@@ -1,5 +1,6 @@
 --The else Statement
  --Add your code here
+--https://www.codecademy.com/courses/learn-lua/lessons/conditionals-logic-Lua/exercises/the-else-statement
  score = 90
  if 80 < score then
  print("You've passed")
