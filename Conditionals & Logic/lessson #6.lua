d91c604cf2fbb13706439872f169ff3c4714004e
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 15:19:17 -0600

    Create lessson #6.lua

---
+++ Conditionals & Logic/lessson #6.lua
@@ -0,0 +1,10 @@
+--Order of Operations
+--https://www.codecademy.com/courses/learn-lua/lessons/conditionals-logic-Lua/exercises/order-of-operations
+
+-- You must be hungry to get food. In addition, you must either have at least $10 or have a friend pay.
+isHungry = false
+cash = 5
+friendIsPaying = true
+if isHungry and cash > 10 or friendIsPaying then
+  print("We are getting food!")
+end
