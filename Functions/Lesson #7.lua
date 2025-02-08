aefcf3c1517ad344c4de423455a6797cef65ec58
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 19:32:07 -0600

    Create Lesson #7.lua

---
+++ Functions/Lesson #7.lua
@@ -0,0 +1,22 @@
+--Practice With Functions
+
+--https://www.codecademy.com/courses/learn-lua/lessons/lua_functions/exercises/practice-with-functions\
+
+function square(x)
+return x * x
+end
+
+function getSmallerValue(a, b)
+if a < b then
+return a
+end
+return b
+end
+
+function convertMilesToFeet(miles)
+return miles * 5280
+end
+
+function getAreaOfRightTriangle(sideLength)
+return 0.5 * sideLength * sideLength
+end
