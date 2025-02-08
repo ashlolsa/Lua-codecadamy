23fd6ec1b5cf39132f86ad0ea7805ddcc640c3d8
Ash <a.g.biniek@GMAIL.COM>
2025-02-07 19:43:37 -0600

    Create Lesson #8.lua

---
+++ Functions/Lesson #8.lua
@@ -0,0 +1,8 @@
+--Built-In Lua Functions
+
+--https://www.codecademy.com/courses/learn-lua/lessons/lua_functions/exercises/built-in-lua-functions
+
+function printStringLength(input)
+print(string.len(input))
+end
+printStringLength("Hello There!")
